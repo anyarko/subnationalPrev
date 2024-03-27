886638669d12c047c1594e79253c2ea8d08c680d
anyarko <albert.nyarko-agyei@nottingham.ac.uk>
2024-03-27 10:57:11 +0000

    updated scaling function

--- utils.r
+++ utils.r
@@ -124,38 +124,28 @@ posteriorRhoScaling <- function(rho, raw_known_sizes,
   K <- ncol(rho)
   new_rho <- rho
   known_ind <- which(raw_known_sizes > 0)
-
-  if(!is.null(correlation)){
-    for(k in 1:K){
-    for(iter in 1:iterations){
-      l_known_ind <- setdiff(known_ind, k)
-      num_known <- length(l_known_ind)
-      w <- correlation[iter, k, l_known_ind]
-      w[w < 0] <- 0 
-      w <- w * ( num_known / sum(w) )
-      
-      known_sizes <- raw_known_sizes[-k]
-      known_sizes <- known_sizes[which(known_sizes > 0)]
-      
-      C_m  <- log( (1/(num_known)) * 
-                     sum( (exp(rho[iter, l_known_ind]) * w ) / (unlist(known_sizes)/population_size)) )
-      new_rho[iter, k] <- rho[iter, k] - C_m
-      
-    }
-  }
-  return(new_rho)
-  }                        
-  
+    
   for(k in 1:K){
     for(iter in 1:iterations){
       l_known_ind <- setdiff(known_ind, k)
       num_known <- length(l_known_ind)
+
+      if(!is.null(correlation)){
+        w <- correlation[iter, k, l_known_ind]
+        w[w < 0] <- 0 
+        w <- w * ( num_known / sum(w) )
+      }
       
       known_sizes <- raw_known_sizes[-k]
       known_sizes <- known_sizes[which(known_sizes > 0)]
       
-      C_m  <- log( (1/(num_known)) * 
+      if(!is.null(correlation)){
+        C_m  <- log( (1/(num_known)) * 
+                     sum( (exp(rho[iter, l_known_ind]) * w ) / (unlist(known_sizes)/population_size)) )
+      }else{
+        C_m  <- log( (1/(num_known)) * 
                      sum( exp(rho[iter, l_known_ind]) / (unlist(known_sizes)/population_size)) )
+      }
       new_rho[iter, k] <- rho[iter, k] - C_m
     }
   }
